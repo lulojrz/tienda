6404b7381c185d5b214b5ee6fb1e67f669fcae1d
lulojrz <109257773+lulojrz@users.noreply.github.com>
2026-06-14 20:18:59 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 // Colocamos la URL de Railway fija directamente para asegurar la conexión
-const API_URL = "https://backend-admin-production-04cd.up.railway.app";
+const API_URL = "https://mi-backend-java-production.up.railway.app";
 
 const AuthContext = createContext();
 
